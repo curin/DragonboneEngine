a4cfa28ec7a41e88bc4a0d72f15509659516519c
Andrew Davis <curin@dbtgaming.com>
2020-03-05 23:31:23 -0500

    Started ISystem

---
+++ Engine/Dragonbones/ISystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dragonbones
+{
+    public interface ISystem
+    {
+        /// <summary>
+        /// What is the name of this system?
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// Is this system currently active?
+        /// </summary>
+        bool Active { get; set; }
+        /// <summary>
+        /// The priority of the system, used to determine which systems should be run less frequently in order to maintain framerate
+        /// </summary>
+        int Priority { get; }
+        /// <summary>
+        /// If Set to 0, will run based on priority system.
+        /// Otherwise, it represents the number of frames finished between each run
+        /// </summary>
+        int RunReccurenceInterval { get; }
+        /// <summary>
+        /// Is this system currently running
+        /// </summary>
+        bool Running { get; set; }
+    }
+}
